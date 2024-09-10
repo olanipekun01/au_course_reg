56735e15bfb0f2f9cd3aa9f3f27cc2f7313f7ce5
olanipekun01 <oloniluaeyitayo@gmail.com>
2024-09-10 09:11:55 -0700

    l

--- app/models.py
+++ app/models.py
@@ -38,38 +38,47 @@ class Student(models.Model):
         return self.registration_set.all()
     
 class Level(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(blank=True, null=True, max_length=80)
     
     def __str__(self):
         return self.name
     
 class College(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(blank=True, null=True, max_length=500)
 
     def __str__(self):
         return self.name
     
 class Department(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(blank=True, null=True, max_length=500)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+    
+class Programme(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(blank=True, null=True, max_length=500)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Course(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(blank=True, null=True, max_length=500)
     courseCode = models.CharField(blank=True, null=True, max_length=15)
     # courseDescription = models.CharField(blank=True, null=True, max_length=250)
     unit = models.IntegerField(blank=True, null=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    programmes = models.ManyToManyField(Programme, related_name='courses')
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return f"{self.course_code} - {self.course_name}"
 
 class Instructor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -81,7 +90,7 @@ class Instructor(models.Model):
         return self.name
 
 class Registration(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     courses = models.ManyToManyField(Course)
     registration_date = models.DateField(auto_now_add=True)
